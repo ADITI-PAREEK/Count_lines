6e59655c1375d0706d44d80d3d9f4d2b399ab163
ADITI-PAREEK <160484110+ADITI-PAREEK@users.noreply.github.com>
2024-02-26 12:06:08 +0530

    Add files via upload

---
+++ count.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# file path
+file_path="/home/aditi/year.sh"
+
+# wc command to count number of lines
+num_of_lines=`wc --lines < $file_path`
+
+# print lines
+echo "number of lines : $num_of_lines" 
+
+
